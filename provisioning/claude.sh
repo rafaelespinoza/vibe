1f720b895151b46fbedf6b9e9675e14412919926
Björn Wieczoreck <bjoern.wieczoreck@giga-infosystems.com>
2026-07-21 20:40:31 +0200

    Fix claude code installation
    Fixed the typo, `npm_args` instead of `npm-args`

--- provisioning/claude.sh
+++ provisioning/claude.sh
@@ -5,7 +5,7 @@ set -euxo pipefail
 # Set this env var so claude doesn't complain about running as root.
 echo "export IS_SANDBOX=1" >> .bashrc
 
-tool='    "npm:@anthropic-ai/claude-code" = { version = "latest", npm-args = "--ignore-scripts=false" }'
+tool='    "npm:@anthropic-ai/claude-code" = { version = "latest", npm_args = "--ignore-scripts=false" }'
 echo "$tool" >> .config/mise/config.toml
 
 mise install
